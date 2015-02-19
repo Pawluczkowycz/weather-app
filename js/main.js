cf356a106efb62b3b71c735c869b07b33a379323
Andrew Pawluczkowycz <awpawluczkowycz@gmail.com>
2015-02-19 17:13:42 -0500

    updating methods.

--- js/main.js
+++ js/main.js
@@ -3,13 +3,30 @@ var andrew = function(jQuery) {
 		init: function() {
 
 		},
-		the_data: []
+		the_data: [],
+		
+		parseData: function(data) {
+		  var location = data.current_observation.observation_location.full;
+		  var currentTemp = data.current_observation.temp_f;
+		  return currentReadings 
+		} 
 	  };
 	  
 	  var controller = {
 		init: function() {
+		  this.getWeather()
 		  view1.init();
 		  view1.render();
+		},
+		getWeather: function() {
+		  $.ajax({
+			  url: "http://api.wunderground.com/api/49ef740b3b58e7ce/conditions/q/CA/San_Francisco.json",
+			  dataType: 'jsonp'
+		    })
+		    .done( function(data) { model.parseData(data); } )
+			.fail( function(jqXHR, textStatus, errorThrown) {
+				showError(errorThrown);
+			});
 		}
 	  };
 	  
