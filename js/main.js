052230fda590c05c241573643fc4453fcec25d31
Andrew Pawluczkowycz <awpawluczkowycz@gmail.com>
2015-02-20 19:22:17 -0500

    added ten day forecast.

--- js/main.js
+++ js/main.js
@@ -10,22 +10,23 @@ var andrew = function(jQuery) {
 		  var location,
 		      currentTemp,
 			  windchillString,
-			  weather;
+			  weather,
+			  icon;
 			  
 		  location = data.current_observation.display_location.full;
 		  currentTemp = data.current_observation.temperature_string;
 		  windchill = data.current_observation.windchill_string;
 		  weather = data.current_observation.weather;
+		  icon = data.current_observation.icon_url;
 		  
 		  model.the_data.location = location;
 		  model.the_data.currentTemp = currentTemp;
 		  model.the_data.windchill = windchill;
 		  model.the_data.weather = weather;
+		  model.the_data.icon = icon;
 		},
 		saveForecast: function(data) {
-		  the_forecast = data.forecast.txt_forecast;
-		  
-		  console.log(the_forecast);
+		  model.the_forecast = data.forecast.txt_forecast;
 		}
 	  };
 	  
@@ -34,7 +35,6 @@ var andrew = function(jQuery) {
 		  this.getWeather();
 		  this.getForecast();
 		  view1.init();
-		  view1.render();
 		},
 		getWeather: function() {
 		  $.ajax({
@@ -51,38 +51,82 @@ var andrew = function(jQuery) {
 			  url: "http://api.wunderground.com/api/49ef740b3b58e7ce/forecast10day/q/VA/Ashburn.json",
 			  dataType: 'jsonp'
 		    })
-		    .done( function(data) { model.saveForecast(data); } )
+		    .done( function(data) {
+				model.saveForecast(data);
+				view2.init();
+			} )
 			.fail( function(jqXHR, textStatus, errorThrown) {
 				showError(errorThrown);
 			});
 		},
 		getTheData: function() {
 		  return model.the_data;
+		},
+		getTheForecast: function() {
+		  return model.the_forecast;
 		}
 	  };
 	  
+	  // Current Conditions
 	  var view1 = {
 		init: function() {
-		  var $getBtn = $('#get-btn'),
-		      $locationSpan = $('#location'),
-			  $weatherSpan = $('#weather'),
-			  $windchillSpan = $('#windchill'),
-			  $currentTempSpan = $('#temp-current');
-			  
-		  $getBtn.on('click', function() {
-				var data = controller.getTheData();
-				$locationSpan.text(data.location);
-				$currentTempSpan.text(data.currentTemp);
-				$weatherSpan.text(data.weather);
-				$windchillSpan.text('Windchill: ' + data.windchill);
-			});
-		},
-		render: function() {
+		  this.getBtn = $('#get-btn');
+		  this.locationSpan = $('#location');
+		  this.weatherSpan = $('#weather');
+		  this.windchillSpan = $('#windchill');
+		  this.currentTempSpan = $('#temp-current');
+		  this.icon = $('#icon');
+		  this.getTodayBtn = $('#get-today');
 		  
+		  view1.render();
+		},
+		
+		render: function() {
+		  this.getBtn.on('click', function() {
+		  view1.data = controller.getTheData();
+		  view1.icon.attr('src', view1.data.icon);
+		  view1.locationSpan.text(view1.data.location);
+		  view1.currentTempSpan.text(view1.data.currentTemp);
+		  view1.weatherSpan.text(view1.data.weather);
+		  view1.windchillSpan.text('Windchill: ' + view1.data.windchill);
+		  });
+		}
+	  };
+	  
+	  // Ten Day Forecast
+	  var view2 = {
+		init: function() {		  
+		  this.getTenDaysBtn = $('#get-ten-btn');
+		  var link = $('#ten_day-forecast');
+		  view2.data = controller.getTheForecast();		
+		  var days = view2.data.forecastday;
+		  function fillForecast(index) {
+			  var $title = $('<span></span>'),
+				  $icon = $('<img />'),
+				  $text = $('<p></p>'),
+			      $div = $('<div class="days"></div>');
+			  $title.text(index.title);
+			  $icon.attr('src', index.icon_url);
+			  $text.text( index.fcttext);
+			  
+			  var temp = $($title).append($icon).append($text);
+			  $div.append(temp);
+			  link.append($div);
+		  }
+			
+		  days.forEach(fillForecast);
+		  
+		  view2.render();
+		},
+		
+		render: function() {		  
+		  this.getTenDaysBtn.on('click', function() {
+			$('.days').fadeToggle();
+			$('#current-conditions').children().slideToggle();
+		  });
 		}
 	  };
 	  
 	  controller.init();
-	  
 };
 andrew(jQuery);
